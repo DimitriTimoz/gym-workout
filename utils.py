b2795255e65e0828a854da7687cff4e49008da0f
Dimitri <dimitri.timoz@orange.fr>
2025-07-20 22:14:45 +0200

    Add initial implementation of CarRacing environment and memory management class

---
+++ utils.py
@@ -0,0 +1,22 @@
+import torch 
+import random
+
+class Memory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, transition):
+        if len(self.memory) < self.capacity:
+            self.memory.append(transition)
+        else:
+            self.memory[self.position] = transition
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size) -> list:
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
